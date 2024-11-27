ac161045e3235bbf3f0a0ef890f53f8b01ed96ca
codEWard-edward <edward.lalande@epitech.eu>
2024-11-27 13:29:33 +0100

    feat(DateTimeServices): cron jobs installed, init and launch, we need now to store actions in database

--- Backend/Services/DateTimeServices/main.go
+++ Backend/Services/DateTimeServices/main.go
@@ -2,12 +2,44 @@ package main
 
 import (
 	"date-time-service/routes"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/robfig/cron"
 )
+
+func BackUpLocalDataCall() {
+	resp, err := http.Get("https://timeapi.io/api/time/current/zone?timeZone=Europe%2FParis")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error while calling the API:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading the response body:", err)
+		return
+	}
+
+	fmt.Println(string(body))
+}
+
+func InitCronScheduler() *cron.Cron {
+	c := cron.New()
+
+	c.AddFunc("@every 00h00m25s", BackUpLocalDataCall)
+
+	c.Start()
+	return c
+}
 
 func main() {
 	r := gin.Default()
+	c := InitCronScheduler()
 
 	r.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
@@ -26,4 +58,5 @@ func main() {
 	routes.ApplyRoutes(r)
 
 	r.Run(":8082")
+	defer c.Stop()
 }
