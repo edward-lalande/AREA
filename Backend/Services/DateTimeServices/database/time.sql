5b856c0bacc64e9698041e86f2f939a40b17caf7
codEWard-edward <edward.lalande@epitech.eu>
2024-11-27 15:14:47 +0100

    feat(DateTimeServices): pgx conn added to the go project to open the database

--- Backend/Services/DateTimeServices/database/time.sql
+++ Backend/Services/DateTimeServices/database/time.sql
@@ -1,5 +1,7 @@
 CREATE TABLE "Action" (
     id           SERIAL NOT NULL PRIMARY KEY,
     time         VARCHAR(32),
+    continent    VARCHAR(32),
+    city         VARCHAR(32),
     user_mail    VARCHAR(32)
 );
