6e0867f9e278aff55723663c42847ebb180ea58d
codEWard-edward <edward.lalande@epitech.eu>
2024-11-27 15:21:24 +0100

    feat(DateTimeModels): Models for the data receive by the frontend and the searching in the database when the cron jobs is set

--- Backend/Services/DateTimeServices/database/time.sql
+++ Backend/Services/DateTimeServices/database/time.sql
@@ -1,7 +1,8 @@
 CREATE TABLE "Action" (
     id           SERIAL NOT NULL PRIMARY KEY,
-    time         VARCHAR(32),
+    user_mail    VARCHAR(32)
     continent    VARCHAR(32),
     city         VARCHAR(32),
-    user_mail    VARCHAR(32)
+    hour         VARCHAR(32),
+    minute       VARCHAR(32)
 );
