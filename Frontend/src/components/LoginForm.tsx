421864dcdce3daa27500403573dd6f10e97cad00
codEWard-edward <edward.lalande@epitech.eu>
2024-11-27 10:44:16 +0100

    feat(BackendFrontend): front is now link to the backend for the login and sign up

--- Frontend/src/components/LoginForm.tsx
+++ Frontend/src/components/LoginForm.tsx
@@ -5,8 +5,30 @@ import { AreaTextDivider } from "./elements/AreaDivider";
 import { AreaTextField } from "./elements/AreaTextFiled";
 import { AreaTypography } from "./elements/AreaTypography";
 import { AreaButton, GoogleButton } from "./elements/AreaButton";
+import { useState } from "react";
+import axios from "axios";
+
+async function loginMe(email: string, password: string) {
+	const url = "http://127.0.0.1:8080/user"
+	const body = {
+		routes: "login",
+		mail: email,
+		password
+	}
+	axios.post(url, body).then((rep) => {
+		console.log(rep)
+		window.location.href = "/";
+	}).catch((e) => {
+		console.error(e)
+	})
+
+}
+
 
 const LoginForm: React.FC = () => {
+
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
 
 	return (
 
@@ -18,11 +40,11 @@ const LoginForm: React.FC = () => {
 				<AreaTypography variant="h4" text="Log in" />
 
 				<AreaBox sx={{ width: "100%", maxWidth: 400, mb: 2 }}>
-					<AreaTextField label="Email" />
-					<AreaTextField label="Password" type="password" />
+					<AreaTextField label="Email" onChange={(s) => setEmail(s.target.value)}/>
+					<AreaTextField label="Password" type="password" onChange={(s) => setPassword(s.target.value)} />
 				</AreaBox>
 
-				<AreaButton text="Log in" />
+				<AreaButton text="Log in" onClick={() => loginMe(email, password)}/>
 
 				<AreaTextDivider text="or" />
 
