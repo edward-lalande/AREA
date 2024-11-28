343b560d507398d274990921ad57f5f82c3ffdb7
edward-lalande <edward.lalande@epitech.eu>
2024-11-28 19:01:27 +0100

    feat(DiscordServer): Discord Server is setup with a ping call to check if he is up and ready

---
+++ Backend/Services/Discord/routes/ApplyRoutes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ApplyRoutes(r *gin.Engine) {
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"ping": "pong"})
+	})
+}
