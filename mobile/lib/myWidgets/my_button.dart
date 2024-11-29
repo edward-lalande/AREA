cad35a6a362e6b7dd8a3c69fd1784f757a4d7485
Timothée Plisson <timothee.plisson@epitech.eu>
2024-11-29 11:44:38 +0100

    feat(mobile): customs widgets (button)

--- mobile/lib/myWidgets/my_button.dart
+++ mobile/lib/myWidgets/my_button.dart
@@ -1,33 +1,48 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  const MyButton({super.key, required this.title});
+  const MyButton({super.key, required this.title, required this.backgroundColor,
+  required this.textColor, required this.padding});
   final String title;
+
+  final Color backgroundColor;
+  final Color textColor;
+  final EdgeInsets padding;
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-          style: ButtonStyle(
-            backgroundColor: WidgetStateProperty.all<Color>(Colors.black),
-            foregroundColor: WidgetStateProperty.all<Color>(const Color.fromARGB(255, 255, 255, 255)),
-            overlayColor: WidgetStateProperty.resolveWith<Color?>(
-              (Set<WidgetState> states) {
-                if (states.contains(WidgetState.hovered)) {
-                  return const Color.fromARGB(255, 112, 112, 112).withOpacity(0.12);
-                }
-                if (states.contains(WidgetState.focused) ||
-                    states.contains(WidgetState.pressed)) {
-                  return const Color.fromARGB(255, 112, 112, 112).withOpacity(0.12);
-                }
-                return null; // Defer to the widget's default.
-              },
+    return Container(
+
+      color: Colors.white,
+      width: double.infinity,
+
+      child: Padding(
+
+        padding: padding,
+        child: TextButton(
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all<Color>(backgroundColor),
+              foregroundColor: WidgetStateProperty.all<Color>(textColor),
+              overlayColor: WidgetStateProperty.resolveWith<Color?>(
+                (Set<WidgetState> states) {
+                  if (states.contains(WidgetState.hovered)) {
+                    return const Color.fromARGB(255, 255, 255, 255).withOpacity(0.24);
+                  }
+                  if (states.contains(WidgetState.focused) ||
+                      states.contains(WidgetState.pressed)) {
+                    return const Color.fromARGB(255, 255, 255, 255).withOpacity(0.24);
+                  }
+                  return null;
+                },
+              ),
             ),
-          ),
-          onPressed: () { },
-          child: Text(
-            title,
-            style: const TextStyle(
-              fontFamily: "Avenir",
-              fontSize: 20),),
-      );
+            onPressed: () { },
+            child: Text(
+              title,
+              style: const TextStyle(
+                fontFamily: "Avenir",
+                fontSize: 20),),
+        ),
+      ),
+    );
   }
 }
