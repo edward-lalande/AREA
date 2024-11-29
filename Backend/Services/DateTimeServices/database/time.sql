71ab8d6df23a46c0b9d5a75987b10ff5f1003cfb
codEWard-edward <edward1.lalande@epitech.eu>
2024-11-29 17:54:28 +0100

    fix(DateTimeSevices): Action of the database take a reaction_identifyer to know to how the reaction is rely to

--- Backend/Services/DateTimeServices/database/time.sql
+++ Backend/Services/DateTimeServices/database/time.sql
@@ -1,8 +1,9 @@
 CREATE TABLE "Action" (
-    id           SERIAL NOT NULL PRIMARY KEY,
-    user_mail    VARCHAR(32),
-    continent    VARCHAR(32),
-    city         VARCHAR(32),
-    hour         INT,
-    minute       INT
+    id                  SERIAL NOT NULL PRIMARY KEY,
+    user_mail           VARCHAR(32),
+    continent           VARCHAR(32),
+    city                VARCHAR(32),
+    hour                INT,
+    minute              INT
+    reaction_identifyer INT,
 );
