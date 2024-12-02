6a481fba4aa97f13723f0043d7db15b81823020e
Timothée Plisson <timothee.plisson@epitech.eu>
2024-12-02 11:15:31 +0100

    feat(mobile): controller state for seing selected char, add obscur text to password

--- mobile/lib/myWidgets/my_text_fields.dart
+++ mobile/lib/myWidgets/my_text_fields.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class MyTextField extends StatelessWidget {
   const MyTextField({super.key, required this.hintText, required this.bgColor,
   required this.fieldBgColor, required this.hintTextColor, required this.inputColor,
-  required this.padding, required this.prefixIcon});
+  required this.padding, required this.prefixIcon, required this.obscureText, required this.controller});
 
   final String hintText;
   final Color hintTextColor;
@@ -12,6 +12,8 @@ class MyTextField extends StatelessWidget {
   final Color fieldBgColor;
   final EdgeInsets padding;
   final Widget prefixIcon;
+  final bool obscureText;
+  final controller;
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +24,15 @@ class MyTextField extends StatelessWidget {
         padding: padding,
         child: Container(
           decoration: BoxDecoration(
+            border: Border.all(color: Colors.black, width: 2),
             borderRadius: BorderRadius.circular(12),
             color: fieldBgColor,
           ),
           child: TextField(
+            controller: controller,
+            obscureText: obscureText,
             textAlignVertical: TextAlignVertical.center,
-            style: TextStyle(color: inputColor),
+            style: TextStyle(color: inputColor, fontFamily: "Avenir"),
             decoration: InputDecoration(
               border: InputBorder.none,
               prefixIcon: prefixIcon,
