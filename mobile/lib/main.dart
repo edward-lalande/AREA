4fc2f55b5ac3c3ed4e62909affbacb7210290b50
Timothée Plisson <timothee.plisson@epitech.eu>
2024-12-02 11:18:06 +0100

    feat(mobile): main with root, its temporary

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'pages/home_page.dart';
 import 'pages/login_page.dart';
+import 'pages/password_page.dart';
+import 'pages/signup_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +14,41 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      title: "training pure ici",
-      home : LoginPage()
+      routerConfig: appRouter(context),
     );
   }
+}
+
+GoRouter appRouter(BuildContext context) {
+  return GoRouter(
+    routes: [
+       GoRoute(
+        path: '/',
+        builder: (context, state) => LoginPage(),
+      ),
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => LoginPage(),
+      ),
+      GoRoute(
+        path: '/home',
+        builder: (context, state) => const HomePage(),
+      ),
+      GoRoute(
+        path: '/password',
+        builder: (context, state) => PasswordPage(),
+      ),
+      GoRoute(
+        path: '/signup',
+        builder: (context, state) => SignUpPage(),
+      ),
+    ],
+     errorBuilder: (context, state) => const  Scaffold(
+      body: Center(
+        child: Text('Page introuvable'),
+      ),
+    ),
+    );
 }
