13d9b3e104cc07ab60a853f4a10d2b0a9c875792
codEWard-edward <edward1.lalande@epitech.eu>
2024-12-07 13:10:15 +0100

    fix(ApiGateway): time models to send to date time api has been changed with the refacto

--- Backend/ApiGateway/Models/TimeModel.go
+++ Backend/ApiGateway/Models/TimeModel.go
@@ -5,29 +5,10 @@ type DateTimeResponse struct {
 	Datetime string `json:"datetime"`
 }
 
-type TimeDataReceive struct {
-	Routes            string `json:"routes"`
-	Token             string `json:"token"`
-	City              string `json:"city"`
-	Continent         string `json:"continent"`
-	Hour              int    `json:"hour"`
-	Minute            int    `json:"minute"`
-	ReactionType      int    `json:"reaction_type"`
-	ReactionServiceId int    `json:"reaction_service_id"`
-	ServerId          string `json:"server_id"`
-	ChannelId         string `json:"channel_id"`
-	Message           string `json:"message"`
-}
-
-type TimeDataToSend struct {
-	Token             string `json:"token"`
-	City              string `json:"city"`
-	Continent         string `json:"continent"`
-	Hour              int    `json:"hour"`
-	Minute            int    `json:"minute"`
-	ReactionType      int    `json:"reaction_type"`
-	ReactionServiceId int    `json:"reaction_service_id"`
-	ServerId          string `json:"server_id"`
-	ChannelId         string `json:"channel_id"`
-	Message           string `json:"message"`
+type TimeActionSend struct {
+	AreaId    string `json:"area_id"`
+	City      string `json:"city"`
+	Continent string `json:"continent"`
+	Hour      int    `json:"hour"`
+	Minute    int    `json:"minute"`
 }
