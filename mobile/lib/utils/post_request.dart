86bc075f711d315a3f9ece3f658855800a8ca0b8
Timothée Plisson <timothee.plisson@epitech.eu>
2024-12-09 13:47:52 +0100

    feat(mobile): get request and navigate to a getUrl (OAuth2 url)

--- mobile/lib/utils/post_request.dart
+++ mobile/lib/utils/post_request.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-Future<bool> sendSignUp({Map<String, dynamic>? body}) async {
+Future<bool> sendSignUp({Map<String, dynamic>? body}) async
+{
 
     final response = await http.post(
         Uri.parse('http://127.0.0.1:8080/user'),
@@ -11,5 +14,41 @@ Future<bool> sendSignUp({Map<String, dynamic>? body}) async {
         return true;
     } else {
         return false;
+    }
+}
+
+
+Future<String> getOAuthUrl(String apiUrl) async
+{
+    try {
+
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        return response.body;
+      }
+      else {
+        throw Exception('Failed to load data: ${response.statusCode}');
+      }
+
+    } catch (e) {
+
+      throw Exception('Error fetching data: $e');
+
+    }
+}
+
+//class exeption + secure+strorage
+
+void logByOAuth(BuildContext context, String apiUrl) async
+{
+    try {
+        String fetchedUrl = await getOAuthUrl(apiUrl);
+
+        if (context.mounted) {
+            context.go(fetchedUrl);
+        }
+    } catch (e) {
+        print("ERRORRRR");
     }
 }
