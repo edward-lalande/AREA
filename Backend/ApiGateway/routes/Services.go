b5ff1cb3c8fb812898341192f33215fbe71ef1cf
codEWard-edward <edward1.lalande@epitech.eu>
2024-12-10 12:03:44 +0100

    fix(ApiGateway): send services upped with a name to display

--- Backend/ApiGateway/routes/Services.go
+++ Backend/ApiGateway/routes/Services.go
@@ -2,31 +2,36 @@ package routes
 
 import (
 	"api-gateway/utils"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 type serviceList struct {
-	name  string
-	url   string
-	color string
+	Name             string `json:"name"`
+	CallToApiGateway string `json:"call_to_api_gateway"`
+	Url              string `json:"url"`
+	Color            string `json:"color"`
 }
 
 func Services(c *gin.Context) {
 	var servicesArray []serviceList
 	var uppedServices []serviceList
 
-	servicesArray = append(servicesArray, serviceList{"Date Time Services", utils.GetEnvKey("TIME_API"), "white"})
-	servicesArray = append(servicesArray, serviceList{"Discord Services", utils.GetEnvKey("DISCORD_API"), "purple"})
+	servicesArray = append(servicesArray, serviceList{"Date Time Services", "time", utils.GetEnvKey("TIME_API"), "#7289da"})
+	servicesArray = append(servicesArray, serviceList{"Discord Services", "discord", utils.GetEnvKey("DISCORD_API"), "purple"})
 
 	for _, service := range servicesArray {
-		_, err := http.Get(service.url + "ping")
+		fmt.Println("call: ", service.Url+"ping")
+		_, err := http.Get(service.Url + "ping")
 		if err != nil {
 			continue
 		}
+		fmt.Println("service append: ", service)
 		uppedServices = append(uppedServices, service)
 	}
+	fmt.Println("upped services: ", uppedServices)
 
-	c.JSON(http.StatusOK, uppedServices)
+	c.JSON(http.StatusOK, gin.H{"services": uppedServices})
 }
