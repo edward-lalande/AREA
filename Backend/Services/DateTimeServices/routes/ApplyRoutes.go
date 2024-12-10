7e13950a72ed1d469b8db9b05789d1062977af30
codEWard-edward <edward1.lalande@epitech.eu>
2024-12-10 17:14:37 +0100

    feat(DateTimeServices): get actions documentation on the swagger and add it to the run of the services

--- Backend/Services/DateTimeServices/routes/ApplyRoutes.go
+++ Backend/Services/DateTimeServices/routes/ApplyRoutes.go
@@ -1,15 +1,20 @@
 package routes
 
 import (
+	_ "date-time-service/docs"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func ApplyRoutes(r *gin.Engine) {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"ping": "pong"})
 	})
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	r.GET("/actions", GetActions)
 
--- Backend/Services/DateTimeServices/routes/GetActions.go
+++ Backend/Services/DateTimeServices/routes/GetActions.go
@@ -7,6 +7,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Get Actions of Date Time services
+// @Summary Get Actions from Date Time services
+// @Description Get Actions from Date Time services
+// @Tags Actions Date Time services
+// @Accept json
+// @Produce json
+// @Success 200 {object} models.GetTimeAction "Reactions name with parameters of it as object"
+// @Router /actions [get]
 func GetActions(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"actions": gin.H{"At time": models.GetTimeAction{}}})
 }
