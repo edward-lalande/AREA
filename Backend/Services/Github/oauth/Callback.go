374151096549d4a417235c24b8d743ca576c2657
edward-lalande <edward.lalande@epitech.eu>
2024-12-16 17:30:02 +0100

    feat(Github): adding callback routes to let the Github oauth call our backend

---
+++ Backend/Services/Github/oauth/Callback.go
@@ -0,0 +1,25 @@
+package oauth
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Github OAUTH2
+// @Summary Send the code received by Github to the frontend
+// @Description Send the code received by Github to the frontend
+// @Tags Github OAUTH2
+// @Accept json
+// @Produce json
+// @Success 200 {object} map[string]string "the code to redirect to"
+// @Router /callback [get]
+func CallBack(c *gin.Context) {
+	code, exists := c.GetQuery("code")
+	if !exists {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Missing code"})
+		return
+	}
+	redirectURL := "http://localhost:8081/login?github_code=" + code
+	c.Redirect(http.StatusFound, redirectURL)
+}
