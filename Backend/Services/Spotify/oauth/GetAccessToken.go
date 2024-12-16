281a864802cd3fca19b5f6e377bcc430e122f6be
edward-lalande <edward.lalande@epitech.eu>
2024-12-16 16:44:12 +0100

    feat(Spotify): get access token routes to get the access token of the user in spotify

---
+++ Backend/Services/Spotify/oauth/GetAccessToken.go
@@ -0,0 +1,47 @@
+package oauth
+
+import (
+	"io"
+	"net/http"
+	"net/url"
+	"spotify/models"
+	"spotify/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Discord OAUTH2
+// @Summary Get
+// @Description Send the code received by the frontend to get the discord access-token of the user
+// @Tags Discord OAUTH2
+// @Accept json
+// @Produce json
+// @Params object models.OauthInformation true "The code must be send as object and the token is not necessary, it can be null"
+// @Success 200 {object} map[string]string "the code to redirect to"
+// @Router /access-token [post]
+func GetAccessToken(c *gin.Context) {
+	var receivedData models.OauthInformation
+
+	if err := c.ShouldBindJSON(&receivedData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	accessTokenUrl := "https://accounts.spotify.com/api/token"
+	data := url.Values{}
+	data.Set("client_id", utils.GetEnvKey("CLIENT_ID"))
+	data.Set("client_secret", utils.GetEnvKey("CLIENT_SECRET"))
+	data.Set("redirect_uri", utils.GetEnvKey("REDIRECT_URI"))
+	data.Set("code", receivedData.Code)
+	data.Set("grant_type", "authorization_code")
+
+	rep, _ := http.PostForm(accessTokenUrl, data)
+	respBody, err := io.ReadAll(rep.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(rep.StatusCode, gin.H{
+		"body": utils.BytesToJson(respBody),
+	})
+}
