5a34e5e658bd10c71dd14766b89cc62fc273ab69
edward-lalande <edward.lalande@epitech.eu>
2024-12-17 20:44:01 +0100

    feat(ApiGateway): adding send spotify actions to the apiGateway to send actions that concer Spotify to the services Spotify

---
+++ Backend/ApiGateway/routes/Spotify.go
@@ -0,0 +1,28 @@
+package routes
+
+import (
+	models "api-gateway/Models"
+	"api-gateway/utils"
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SendSpotifyActions(data models.SpotifyActions, c *gin.Context) *http.Response {
+	jsonBody, err := json.Marshal(data)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return nil
+	}
+
+	resp, err := http.Post(utils.GetEnvKey("SPOTIFY_API")+"action", "application/jsons", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return nil
+	}
+	defer resp.Body.Close()
+
+	return resp
+}
