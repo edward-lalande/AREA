564accb061c0fcf6173f92f4d4931d6be7180f94
edward-lalande <edward.lalande@epitech.eu>
2024-12-17 20:40:15 +0100

    feat(ApiGateway): Spotify models to the ApiGateway to send area

--- Backend/ApiGateway/Models/AreaModel.go
+++ Backend/ApiGateway/Models/AreaModel.go
@@ -15,6 +15,14 @@ type GitlabAction struct {
 	AreaId     string `json:"area_id"`
 }
 
+type SpotifyActions struct {
+	AreaId      string `json:"area_id"`
+	ActionType  int    `json:"action_type"`
+	IsPlaying   int    `json:"is_playing"`
+	AccessToken string `json:"user_token"`
+	MusicName   string `json:"music_name"`
+}
+
 type TypeTimeAction struct {
 	BaseAction
 	// ActionType of dateTime
