7cb0a4c7f23b707c8e514ecda521f28377ecda07
edward-lalande <edward.lalande@epitech.eu>
2024-12-17 20:43:28 +0100

    feat(Spotify): adding get Actions to Spotify services to send it to the api gateway

--- Backend/Services/Spotify/oauth/OauthFront.go
+++ Backend/Services/Spotify/oauth/OauthFront.go
@@ -19,6 +19,6 @@ func OAuthFront(c *gin.Context) {
 	authUrl := "https://accounts.spotify.com/authorize?&client_id=" + utils.GetEnvKey("CLIENT_ID") +
 		"&redirect_uri=" + utils.GetEnvKey("REDIRECT_URI") +
 		"&response_type=code" +
-		"&scope=user-read-private%20user-read-email"
+		"&scope=user-library-read%20user-library-modify%20user-read-recently-played%20user-top-read%20user-read-playback-position%20user-follow-modify%20playlist-modify-public%20playlist-modify-private%20playlist-read-collaborative%20playlist-read-private%20streaming%20user-read-currently-playing%20user-modify-playback-state%20user-read-playback-state%20user-read-email%20user-follow-read"
 	c.String(http.StatusOK, authUrl)
 }
--- Backend/Services/Spotify/routes/ApplyRoutes.go
+++ Backend/Services/Spotify/routes/ApplyRoutes.go
@@ -2,7 +2,9 @@ package routes
 
 import (
 	"net/http"
+	area "spotify/Area"
 	"spotify/oauth"
+	"spotify/utils"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,8 +18,16 @@ func ApplyRoutes(r *gin.Engine) {
 	r.GET("/callback", oauth.CallBack)
 	r.POST("/access-token", oauth.GetAccessToken)
 
+	r.POST("/action", area.Actions)
+
 	r.GET("/actions", func(c *gin.Context) {
-		c.JSON(http.StatusOK, nil)
+		b, err := utils.OpenFile("Models/Actions.json")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		json := utils.BytesToJson(b)
+		c.JSON(http.StatusOK, json)
 	})
 
 	r.GET("/reactions", func(c *gin.Context) {
