0a39ce8c16af02c99966164a446f1de32e1848a5
Paul BARDEUR <paul.bardeur@epitech.eu>
2024-12-18 11:16:13 +0100

    feat(Twilio): set routes to get actions and reactions of twilio and create a reaction on database

--- Backend/Services/Twilio/routes/ApplyRoutes.go
+++ Backend/Services/Twilio/routes/ApplyRoutes.go
@@ -7,15 +7,15 @@ import (
 )
 
 func ApplyRoutes(r *gin.Engine) {
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"ping": "pong"})
 	})
 
-	r.GET("/actions", func(c *gin.Context) {
-		c.JSON(http.StatusOK, nil)
-	})
+	r.GET("/actions", GetActions)
+	r.GET("/reactions", GetReactions)
 
-	r.GET("/reactions", func(c *gin.Context) {
-		c.JSON(http.StatusOK, nil)
-	})
+	r.POST("/reaction", Reaction)
+
+	r.POST("/trigger", Trigger)
 }
