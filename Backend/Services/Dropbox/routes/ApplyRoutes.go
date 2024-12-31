427360003527e15b8e039c0dd6df5e03e95ffa46
edward-lalande <edward.lalande@epitech.eu>
2024-12-31 18:24:16 +0100

    feat(DropBox): Routes of trigger and store Reactions handling

--- Backend/Services/Dropbox/routes/ApplyRoutes.go
+++ Backend/Services/Dropbox/routes/ApplyRoutes.go
@@ -1,7 +1,9 @@
 package routes
 
 import (
+	area "dropbox/Area"
 	"dropbox/oauth"
+	"dropbox/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -16,9 +18,18 @@ func ApplyRoutes(r *gin.Engine) {
 		c.JSON(http.StatusOK, nil)
 	})
 
+	r.POST("/reaction", area.StoreReactions)
 	r.GET("/reactions", func(c *gin.Context) {
-		c.JSON(http.StatusOK, nil)
+		b, err := utils.OpenFile("Models/Reactions.json")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		json := utils.BytesToJson(b)
+		c.JSON(http.StatusOK, json)
 	})
+
+	r.POST("/trigger", area.Trigger)
 
 	r.GET("/oauth", oauth.OAuthFront)
 	r.GET("/callback", oauth.CallBack)
