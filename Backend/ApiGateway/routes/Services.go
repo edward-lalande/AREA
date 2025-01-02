6e84c95a233b1ccd9b27db57e66f7008a435a36f
edward-lalande <edward.lalande@epitech.eu>
2025-01-02 15:48:57 +0100

    feat(Asana): Sending actual available reactions to the api gateway

--- Backend/ApiGateway/routes/Services.go
+++ Backend/ApiGateway/routes/Services.go
@@ -14,6 +14,11 @@ type serviceList struct {
 	Color            string `json:"color"`
 }
 
+type serviceSendList struct {
+	Name  string `json:"name"`
+	Color string `json:"color"`
+}
+
 // Get Up services
 // @Summary Get all services up
 // @Description Get services up with name to display, routes to call it to the api-gateway, url and color to display
@@ -25,26 +30,26 @@ type serviceList struct {
 // @Router /services [get]
 func Services(c *gin.Context) {
 	servicesArray := []serviceList{
-		{"Date Time Services", "time", utils.GetEnvKey("TIME_API"), "#b3b3b3"},
-		{"Discord Services", "discord", utils.GetEnvKey("DISCORD_API"), "#7289da"},
-		{"Dropbox", "dropbox", utils.GetEnvKey("DROPBOX_API"), "#7289da"},
-		{"Github", "github", utils.GetEnvKey("GITHUB_API"), "#7289da"},
-		{"Gitlab", "gitlab", utils.GetEnvKey("GITLAB_API"), "#7289da"},
-		{"Google", "google", utils.GetEnvKey("GOOGLE_API"), "#7289da"},
-		{"Meteo", "meteo", utils.GetEnvKey("METEO_API"), "#7289da"},
-		{"Spotify", "spotify", utils.GetEnvKey("SPOTIFY_API"), "#7289da"},
-		{"Asana", "asana", utils.GetEnvKey("ASANA_API"), "#7289da"},
-		{"Ticket Master", "ticket-master", utils.GetEnvKey("TICKET_MASTER_API"), "#7289da"},
-		{"Twilio", "twilio", utils.GetEnvKey("TWILIO_API"), "#7289da"},
+		{"Date Time Service", "time", utils.GetEnvKey("TIME_API"), "#b3b3b3"},
+		{"Discord Service", "discord", utils.GetEnvKey("DISCORD_API"), "#7289da"},
+		{"Dropbox Service", "dropbox", utils.GetEnvKey("DROPBOX_API"), "#7289da"},
+		{"Github Service", "github", utils.GetEnvKey("GITHUB_API"), "#7289da"},
+		{"Gitlab Service", "gitlab", utils.GetEnvKey("GITLAB_API"), "#7289da"},
+		{"Google Service", "google", utils.GetEnvKey("GOOGLE_API"), "#7289da"},
+		{"Meteo Service", "meteo", utils.GetEnvKey("METEO_API"), "#7289da"},
+		{"Spotify Service", "spotify", utils.GetEnvKey("SPOTIFY_API"), "#7289da"},
+		{"Asana Service", "asana", utils.GetEnvKey("ASANA_API"), "#7289da"},
+		{"Ticket Master Service", "ticket-master", utils.GetEnvKey("TICKET_MASTER_API"), "#7289da"},
+		{"Twilio Service", "twilio", utils.GetEnvKey("TWILIO_API"), "#7289da"},
 	}
-	var uppedServices []serviceList
+	var uppedServices []serviceSendList
 
 	for _, service := range servicesArray {
 		_, err := http.Get(service.Url + "ping")
 		if err != nil {
 			continue
 		}
-		uppedServices = append(uppedServices, service)
+		uppedServices = append(uppedServices, serviceSendList{service.Name, service.Color})
 	}
 
 	c.JSON(http.StatusOK, gin.H{"services": uppedServices})
--- Backend/Services/Asana/routes/ApplyRoutes.go
+++ Backend/Services/Asana/routes/ApplyRoutes.go
@@ -1,7 +1,9 @@
 package routes
 
 import (
+	area "asana/Area"
 	"asana/oauth"
+	"asana/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -16,12 +18,20 @@ func ApplyRoutes(r *gin.Engine) {
 		c.JSON(http.StatusOK, nil)
 	})
 
-	r.GET("/reactions", func(c *gin.Context) {
-		c.JSON(http.StatusOK, nil)
-	})
-
 	r.GET("/oauth", oauth.OAuthFront)
 	r.GET("/callback", oauth.CallBack)
 	r.POST("/access-token", oauth.GetAccessToken)
 
+	r.GET("/reactions", func(c *gin.Context) {
+		b, err := utils.OpenFile("Models/Reactions.json")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		json := utils.BytesToJson(b)
+		c.JSON(http.StatusOK, json)
+	})
+
+	r.POST("/reaction", area.StoreReactions)
+	r.POST("/trigger", area.Trigger)
 }
