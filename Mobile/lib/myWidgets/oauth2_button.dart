d2094c76caba3d13103029484553bcc72154f374
Timothée Plisson <timothee.plisson@epitech.eu>
2025-01-02 17:50:18 +0100

    feat(mobile): card in login Page redirect for Oauth2

--- Mobile/lib/myWidgets/oauth2_button.dart
+++ Mobile/lib/myWidgets/oauth2_button.dart
@@ -6,34 +6,34 @@ class OauthButton extends StatelessWidget {
         required this.iconPath,
         required this.resize,
         this.resizePadding,
+        this.onPressed,
     });
 
     final String iconPath;
     final bool resize;
     final EdgeInsetsGeometry? resizePadding;
+    final VoidCallback? onPressed;
 
     @override
     Widget build(BuildContext context) {
-        return Container(
-            padding: !resize ? EdgeInsets.only(
-                top: 20,
-                bottom: 20,
-                left: 20,
-                right:  20,
-
-            ) : resizePadding,
-           
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                border: Border.all(
-                    width: 2,
-                    color: Colors.black
+        return GestureDetector(
+            onTap: onPressed,
+            child: Card(
+                elevation:5, // Définit l'élévation
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                ),
+                child: Padding(
+                    padding: !resize
+                        ? const EdgeInsets.all(20)
+                        : resizePadding ?? const EdgeInsets.all(20),
+                    child: Image.asset(
+                        iconPath,
+                        height: 40,
                     ),
                 ),
-                child: Image.asset(
-                    iconPath,
-                    height: 40,
-             ),
+            ),
         );
     }
 }
+
