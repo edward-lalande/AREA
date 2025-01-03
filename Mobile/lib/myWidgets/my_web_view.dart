d0cd5054a73800c220acb859801f8db9672dddcb
Timothée Plisson <timothee.plisson@epitech.eu>
2025-01-03 12:51:51 +0100

    feat(mobile): add request in webview widget for getting all Services and Actions

--- Mobile/lib/myWidgets/my_web_view.dart
+++ Mobile/lib/myWidgets/my_web_view.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -6,10 +8,12 @@ import '../utils/post_request.dart';
 
 class WebViewPage extends StatefulWidget {
     final String url;
+    final String serv;
 
     const WebViewPage({
         super.key,
         required this.url,
+        required this.serv,
     });
     @override
     State<WebViewPage> createState() => _WebViewPageState();
@@ -24,50 +28,80 @@ class _WebViewPageState extends State<WebViewPage> {
     @override
     void initState() {
         super.initState();
+        String servRoot = widget.serv;
         _controller = WebViewController()
         ..setJavaScriptMode(JavaScriptMode.unrestricted)
         ..setNavigationDelegate(
             NavigationDelegate(
-            onPageStarted: (String url) {
-                setState(() {
-                _isLoading = true;
-                });
-            },
-            onPageFinished: (String url) async {
-                setState(() {
-                _isLoading = false;
-                });
-
-                if (url.contains("code=")) {
-                final Uri uri = Uri.parse(url);
-                final String? code = uri.queryParameters["code"];
-
-                if (code != null) {
-
-                    bool tmp = await sendSignUp(
-                        url: "http://10.0.2.2:8083/access-token",
-                        body: {
-                            //"routes": "access-token",
-                            "code": code,
-                        }
+                onPageStarted: (String url) {
+                    setState(() {
+                    _isLoading = true;
+                    });
+                },
+                onPageFinished: (String url) async {
+                    final String servString = await classicGet(
+                        url: "http://10.0.2.2:8080/services",
                     );
-                    if (tmp) {
-                        if (context.mounted) {
-                            context.go("/home");
-                        }
-                    } else {
-                        if (context.mounted) {
-                            context.go("/login");
+                    final String actionsString = await classicGet(
+                        url: "http://10.0.2.2:8080/actions",
+                    );
+                    List<dynamic> data = jsonDecode(actionsString);
+                    actionsMap = {
+                        for (var service in data.where((element) => element != null))
+                            service['name']: {
+                                'actions': service['actions'].map((action) {
+                                    return {
+                                        'name': action['name'],
+                                        'arguments': action['arguments'],
+                                    };
+                                }).toList(),
+                            }
+                    };
+                    servicesMap = jsonDecode(servString);
+                    setState(() {
+                        _isLoading = false;
+                    });
+                    if (url.contains("code=")) {
+
+                        final Uri uri = Uri.parse(url);
+                        final String? code = uri.queryParameters["code"];
+                        if (code != null) {
+                            bool tmp = await sendSignUp(
+                                url: "http://10.0.2.2:8080/$servRoot/access-token",
+                                body: {
+                                //"routes": "access-token",
+                                    "code": code,
+                                }
+                            );
+                            if (tmp) {
+                                if (context.mounted) {
+                                    context.go("/home");
+                                }
+                            } else {
+                                if (context.mounted) {
+                                    context.go("/login");
+                                }
+                            }
                         }
                     }
-                }
-                }
-            },
+                },
             ),
         )
         ..loadRequest(Uri.parse(widget.url));
     }
 
+    String getService(String url)
+    {
+        StringBuffer service = StringBuffer();
+
+        for (var i = url.length - 7; i >= 0; i--) {
+            if (url[i] == '/') {
+                break;
+            }
+            service.write(url[i]);
+        }
+        return service.toString().split('').reversed.join();
+    }
     @override
     Widget build(BuildContext context) {
         return Scaffold(
