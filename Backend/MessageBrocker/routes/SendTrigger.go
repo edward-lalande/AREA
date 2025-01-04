dd51726626cbae0a3940be26496944dd921bdfb1
edward-lalande <edward.lalande@epitech.eu>
2025-01-04 15:40:49 +0100

    feat(MessageBrocker): cryptomoney in the array of service to let Crypto money sending trigger

--- Backend/MessageBrocker/routes/SendTrigger.go
+++ Backend/MessageBrocker/routes/SendTrigger.go
@@ -44,6 +44,7 @@ func Trigger(c *gin.Context) {
 		10: utils.GetEnvKey("ASANA_API"),
 		11: utils.GetEnvKey("TICKET_MASTER_API"),
 		12: utils.GetEnvKey("TWILIO_API"),
+		13: utils.GetEnvKey("CRYPTOMONEY_API"),
 	}
 
 	if err := c.ShouldBindJSON(&receivedData); err != nil {
