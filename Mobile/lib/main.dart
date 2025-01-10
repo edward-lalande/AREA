9d675792a58533fa65c27f341f7dc845f064eaf1
Timothée Plisson <timothee.plisson@epitech.eu>
2025-01-10 10:46:54 +0100

    feat(mobile): change OauthButton for can being tabulation (navigation)

--- Mobile/lib/main.dart
+++ Mobile/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:provider/provider.dart';
+import 'package:second_app/theme/theme_provider.dart';
+import 'package:second_app/theme/themes.dart';
 
 
 import 'pages/home_page.dart';
@@ -16,49 +19,50 @@ void main() {
 final storage = const FlutterSecureStorage();
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+    const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: appRouter(context),
-    );
-  }
+    @override
+     Widget build(BuildContext context) {
+        return MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            routerConfig: appRouter(context),
+        );
+    }
 }
 
 GoRouter appRouter(BuildContext context) {
-  return GoRouter(
-    routes: [
-       GoRoute(
-        path: '/',
-        builder: (context, state) => LoginPage(),
-      ),
-      GoRoute(
-        path: '/login',
-        builder: (context, state) => LoginPage(),
-      ),
-      GoRoute(
-        path: '/host',
-        builder: (context, state) => HostPage(),
-      ),
-      GoRoute(
-        path: '/home',
-        builder: (context, state) => const HomePage(),
-      ),
-      GoRoute(
-        path: '/password',
-        builder: (context, state) => PasswordPage(),
-      ),
-      GoRoute(
-        path: '/signup',
-        builder: (context, state) => const SignUpPage(),
-      ),
-    ],
-     errorBuilder: (context, state) => const  Scaffold(
-      body: Center(
-        child: Text('Page introuvable'),
-      ),
-    ),
+    return GoRouter(
+
+        routes: [
+            GoRoute(
+                path: '/',
+                builder: (context, state) => LoginPage(),
+            ),
+            GoRoute(
+                path: '/login',
+                builder: (context, state) => LoginPage(),
+            ),
+            GoRoute(
+                path: '/host',
+                builder: (context, state) => HostPage(),
+            ),
+            GoRoute(
+                path: '/home',
+                builder: (context, state) => const HomePage(),
+            ),
+            GoRoute(
+                path: '/password',
+                builder: (context, state) => PasswordPage(),
+            ),
+            GoRoute(
+                path: '/signup',
+                builder: (context, state) => const SignUpPage(),
+            ),
+        ],
+        errorBuilder: (context, state) => const  Scaffold(
+            body: Center(
+                child: Text('Page introuvable'),
+            ),
+        ),
     );
 }
--- Mobile/lib/myWidgets/oauth2_button.dart
+++ Mobile/lib/myWidgets/oauth2_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class OauthButton extends StatelessWidget {
+class OauthButton extends StatefulWidget {
     const OauthButton({
         super.key,
         required this.iconPath,
@@ -15,27 +15,54 @@ class OauthButton extends StatelessWidget {
     final void Function(BuildContext)? onPressed;
 
     @override
+    _OauthButtonState createState() => _OauthButtonState();
+}
+
+class _OauthButtonState extends State<OauthButton> {
+    late FocusNode _focusNode;
+
+    @override
+    void initState() {
+        super.initState();
+        _focusNode = FocusNode();
+    }
+
+    @override
+    void dispose() {
+        _focusNode.dispose();
+        super.dispose();
+    }
+
+    @override
     Widget build(BuildContext context) {
-        return GestureDetector(
-            onTap: () {
-                onPressed!(context);
-            },
-            child: Card(
-                elevation:5,
-                shape: RoundedRectangleBorder(
+        return Card(
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+            ),
+            child: Focus(
+                focusNode: _focusNode,
+                child: InkWell(
                     borderRadius: BorderRadius.circular(16),
-                ),
-                child: Padding(
-                    padding: !resize
-                        ? const EdgeInsets.all(20)
-                        : resizePadding ?? const EdgeInsets.all(20),
-                    child: Image.asset(
-                        iconPath,
-                        height: 40,
+                    onTap: () {
+                        if (widget.onPressed != null) {
+                        widget.onPressed!(context);
+                        }
+                    },
+                    onFocusChange: (hasFocus) {
+                        setState(() {});
+                    },
+                    child: Padding(
+                        padding: !widget.resize
+                            ? const EdgeInsets.all(20)
+                            : widget.resizePadding ?? const EdgeInsets.all(20),
+                        child: Image.asset(
+                            widget.iconPath,
+                            height: 40,
+                            ),
                     ),
                 ),
             ),
         );
     }
 }
-
