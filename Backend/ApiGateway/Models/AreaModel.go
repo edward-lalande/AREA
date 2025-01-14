edb1cdd95b20373ddd6c96a38fde907398b36712
Paul BARDEUR <paul.bardeur@epitech.eu>
2025-01-14 18:37:48 +0100

    fix(Backend): add a token on all models oauth2 add token

--- Backend/ApiGateway/Models/AreaModel.go
+++ Backend/ApiGateway/Models/AreaModel.go
@@ -236,3 +236,8 @@ type AreaDatabase struct {
 type OauthCode struct {
 	OauthCode string `json:"code"`
 }
+
+type OauthCodeToken struct {
+	OauthCode string `json:"code"`
+	Token     string `json:"token"`
+}
