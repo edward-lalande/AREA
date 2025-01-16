3a670edd894564c053412cbddcd363bfe611944e
edward-lalande <edward.lalande@epitech.eu>
2025-01-16 15:54:43 +0100

    feat(ApiGateway): oauht and add-oauth routes unit tests

--- Backend/ApiGateway/routes/route_test.go
+++ Backend/ApiGateway/routes/route_test.go
@@ -34,13 +34,122 @@ func TestPingRoute(t *testing.T) {
 	assert.Equal(t, "{\"ping\":\"pong\"}", w.Body.String())
 }
 
-func TestDiscordOauth(t *testing.T) {
+func TestOauth(t *testing.T) {
 	router := gin.Default()
 	routes.ApplyRoutes(router)
+	tests := []struct {
+		Link              string
+		CodeExcepected    int
+		ResponseExcpected string
+	}{
+		{
+			Link:              "/discord/oauth",
+			CodeExcepected:    400,
+			ResponseExcpected: "{\"error\":\"Get \\\"oauth\\\": unsupported protocol scheme \\\"\\\"\"}",
+		},
+		{
+			Link:              "/spotify/oauth",
+			CodeExcepected:    400,
+			ResponseExcpected: "{\"error\":\"Get \\\"oauth\\\": unsupported protocol scheme \\\"\\\"\"}",
+		},
+		{
+			Link:              "/github/oauth",
+			CodeExcepected:    400,
+			ResponseExcpected: "{\"error\":\"Get \\\"oauth\\\": unsupported protocol scheme \\\"\\\"\"}",
+		},
+		{
+			Link:              "/gitlab/oauth",
+			CodeExcepected:    400,
+			ResponseExcpected: "{\"error\":\"Get \\\"oauth\\\": unsupported protocol scheme \\\"\\\"\"}",
+		},
+		{
+			Link:              "/google/oauth",
+			CodeExcepected:    400,
+			ResponseExcpected: "{\"error\":\"Get \\\"oauth\\\": unsupported protocol scheme \\\"\\\"\"}",
+		},
+		{
+			Link:              "/dropbox/oauth",
+			CodeExcepected:    400,
+			ResponseExcpected: "{\"error\":\"Get \\\"oauth\\\": unsupported protocol scheme \\\"\\\"\"}",
+		},
+		{
+			Link:              "/asana/oauth",
+			CodeExcepected:    400,
+			ResponseExcpected: "{\"error\":\"Get \\\"oauth\\\": unsupported protocol scheme \\\"\\\"\"}",
+		},
+		{
+			Link:              "/miro/oauth",
+			CodeExcepected:    400,
+			ResponseExcpected: "{\"error\":\"Get \\\"oauth\\\": unsupported protocol scheme \\\"\\\"\"}",
+		},
+	}
 
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/discord/oauth", nil)
-	router.ServeHTTP(w, req)
 
-	assert.Contains(t, w.Body.String(), "{\"error\":\"Get \\\"oauth\\\": unsupported protocol scheme \\\"\\\"\"}")
+	for _, test := range tests {
+		req, _ := http.NewRequest("GET", test.Link, nil)
+		router.ServeHTTP(w, req)
+		assert.Equal(t, w.Code, test.CodeExcepected)
+		assert.Contains(t, w.Body.String(), test.ResponseExcpected)
+	}
+}
+
+func TestAddOauth(t *testing.T) {
+	router := gin.Default()
+	routes.ApplyRoutes(router)
+	tests := []struct {
+		Link              string
+		CodeExcepected    int
+		ResponseExcpected string
+	}{
+		{
+			Link:              "/discord/add-oauth",
+			CodeExcepected:    400,
+			ResponseExcpected: "{\"error\":\"Get \\\"add-oauth\\\": unsupported protocol scheme \\\"\\\"\"}",
+		},
+		{
+			Link:              "/spotify/add-oauth",
+			CodeExcepected:    400,
+			ResponseExcpected: "{\"error\":\"Get \\\"add-oauth\\\": unsupported protocol scheme \\\"\\\"\"}",
+		},
+		{
+			Link:              "/github/add-oauth",
+			CodeExcepected:    400,
+			ResponseExcpected: "{\"error\":\"Get \\\"add-oauth\\\": unsupported protocol scheme \\\"\\\"\"}",
+		},
+		{
+			Link:              "/gitlab/add-oauth",
+			CodeExcepected:    400,
+			ResponseExcpected: "{\"error\":\"Get \\\"add-oauth\\\": unsupported protocol scheme \\\"\\\"\"}",
+		},
+		{
+			Link:              "/google/add-oauth",
+			CodeExcepected:    400,
+			ResponseExcpected: "{\"error\":\"Get \\\"add-oauth\\\": unsupported protocol scheme \\\"\\\"\"}",
+		},
+		{
+			Link:              "/dropbox/add-oauth",
+			CodeExcepected:    400,
+			ResponseExcpected: "{\"error\":\"Get \\\"add-oauth\\\": unsupported protocol scheme \\\"\\\"\"}",
+		},
+		{
+			Link:              "/asana/add-oauth",
+			CodeExcepected:    400,
+			ResponseExcpected: "{\"error\":\"Get \\\"add-oauth\\\": unsupported protocol scheme \\\"\\\"\"}",
+		},
+		{
+			Link:              "/miro/add-oauth",
+			CodeExcepected:    400,
+			ResponseExcpected: "{\"error\":\"Get \\\"add-oauth\\\": unsupported protocol scheme \\\"\\\"\"}",
+		},
+	}
+
+	w := httptest.NewRecorder()
+
+	for _, test := range tests {
+		req, _ := http.NewRequest("GET", test.Link, nil)
+		router.ServeHTTP(w, req)
+		assert.Equal(t, w.Code, test.CodeExcepected)
+		assert.Contains(t, w.Body.String(), test.ResponseExcpected)
+	}
 }
